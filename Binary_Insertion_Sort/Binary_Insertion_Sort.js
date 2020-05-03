9f61d813927df4a1be3146fbb17ef981148eed44
Sheetal-ssr <42754054+Sheetal-ssr@users.noreply.github.com>
2020-05-04 03:47:37 +0530

    Update Binary_Insertion_Sort.js

--- Binary_Insertion_Sort/Binary_Insertion_Sort.js
+++ Binary_Insertion_Sort/Binary_Insertion_Sort.js
@@ -4,40 +4,45 @@ var i=0;
 //taking the size of the array dynamically
 var n=prompt("enter the size of your array");
 var arr = [];
-for(var i=0; i<n; i++)
+
+for(var i = 0 ; i < n ; i++)
 {
     //Taking Input from user
-    arr[i] = prompt('Enter Element ' + (i+1));
+    arr[i] = prompt('Enter Element ' + (i + 1));
 }
 
 function binarysearch(array,item,start,end)
 {
     if(end <= start)
         return (item > array[start])? (start+1): start;
+    
     var mid = (start+end)/2;
+    
     if(item == array[mid])
         return mid+1;
+    
     if(item >array[mid])
         return binarysearch(array,item,mid+1,end);
+    
     return binarysearch(array,item,start,mid-1);
 }
 
 function insertionsort(array,size)
 {
     var i,location,j,k,key;
-    for(i=1;i<size;++i)
+    for( i = 1 ; i < size ; ++i)
     {
-        j = i-1;
+        j = i - 1;
         key = arr[i];
         //to find location where key sould be inserted
         location = binarysearch(arr,key,0,j);
          // to move all elements after location to create space for key
         while(j >= location)
         {
-            array[j+1] = array[j];
+            array[j + 1] = array[j];
             j--;
         }
-        array[j+1] = key;
+        array[j + 1] = key;
     }
 }
 
